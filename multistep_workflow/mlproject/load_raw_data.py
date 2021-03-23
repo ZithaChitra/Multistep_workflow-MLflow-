86ca80fe133b4747a9db501cf115309857cc8ab5
zithachitra <realblessingchitra@gmail.com>
2021-03-23 07:03:37 +0000

    load data and convert to Parquet functions

--- multistep_workflow/mlproject/load_raw_data.py
+++ multistep_workflow/mlproject/load_raw_data.py
@@ -17,10 +17,25 @@ import click
 def load_raw_data(url):
     with mlflow.start_run() as mlrun:
         local_dir = tempfile.mkdtemp()
-        loacl_filename = os.path.join(local_dir, "ml-20m.zip")
+        local_filename = os.path.join(local_dir, "ml-20m.zip")
         print(f"Downloading {url} to {local_filename}")
         r = requests.get(url, stream=True)
         with open(local_filename, "wb") as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
+
+
+		extracted_dir = os.path.join(local_dir, "ml-20m")
+        print("Extracting %s into %s" % (local_filename, extracted_dir))
+        with zipfile.ZipFile(local_filename, "r") as zip_ref:
+            zip_ref.extractall(local_dir)
+
+        ratings_file = os.path.join(extracted_dir, "ratings.csv")
+
+        print("Uploading ratings: %s" % ratings_file)
+		mlflow.log_artifact(ratings_file, "ratings-csv-dir")
+
+
+if __name__ == "__main__":
+	load_raw_data()
